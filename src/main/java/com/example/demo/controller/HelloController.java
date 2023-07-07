d50fae2bf37defc240a0c315c599c7a806181d3d
Lee <390455946@qq.com>
2023-07-07 10:05:00 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> getHello() {
+        String message = "Hello China!";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+//
+//    @GetMapping("/world")
+//    public ResponseEntity<String> getWorld() {
+//        String message = "Hello World!";
+//        return new ResponseEntity<>(message, HttpStatus.OK);
+//    }
+}
