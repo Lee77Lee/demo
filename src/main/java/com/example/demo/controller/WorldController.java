b59db45ad96eed0625ed0a41fee82375639b2ba1
Lee <390455946@qq.com>
2023-07-24 17:08:12 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/WorldController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class WorldController {
+
+    @GetMapping("/1")
+    public ResponseEntity<String> getHello() {
+        String message = "Hello 1!";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @GetMapping("/2")
+    public ResponseEntity<String> getWorld() {
+        String message = "Hello 2!";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+}
+
